52aa9cac845e666307c396e5e07f4dcab0670a2f
Christian Bergschneider <magiclake@gmx.de>
2023-09-13 12:19:16 +0200

    feat: add initialization of a local dev env

--- src/index.ts
+++ src/index.ts
@@ -125,4 +125,214 @@ if (!init_dev_env) {
     process.exit(0);
 } 
 
-p.log.step("Initializing development environment currently not supported.");
+p.log.step("Initializing development environment");
+
+/*
+WE NEED TO GENERATE THIS ENV FILE: 
+
+# DATABASE
+
+ENABLE_DRIZZLE_LOGGER=true
+
+## MySQL
+
+MYSQL_DB_HOST=
+MYSQL_DB_PORT=
+MYSQL_DB_USER=
+MYSQL_DB_PASSWORD=
+MYSQL_DB_NAME=
+
+## POSTGRES
+POSTGRES_DB_HOST=
+POSTGRES_DB_PORT=
+POSTGRES_DB_USER=
+POSTGRES_DB_PASSWORD=
+POSTGRES_DB_NAME=
+POSTGRES_MAX_CONNECTIONS=
+
+## TURSO
+
+TURSO_DB_URL=
+TURSO_AUTH_TOKEN=
+
+# OAUTH
+
+GITHUB_CLIENT_ID=
+GITHUB_CLIENT_SECRET=
+
+GOOGLE_OAUTH_CLIENT_ID=
+GOOGLE_OAUTH_CLIENT_SECRET=
+GOOGLE_OAUTH_REDIRECT_URI=
+
+# EMAIL
+
+RESEND_API_KEY=
+*/
+
+// We'll just use mysql for now - TODO add postgres support
+
+// Ask if we should use mysql from docker
+let use_mysql_with_docker = await p.confirm({
+    message: 'Do you want to use MySQL from Docker?',
+    initialValue: true
+});
+
+let mysql_db_host = 'localhost';
+let mysql_db_port = '3306';
+let mysql_db_user = 'root';
+let mysql_db_password = 'password';
+let mysql_db_name = 'kitforstartups-mysql';
+
+if (use_mysql_with_docker) {
+    p.log.info("Your MySQL database will be available at " + chalk.bold("mysql://" + mysql_db_host + ":" + mysql_db_port) + " with username " + chalk.bold(mysql_db_user) + " and password " + chalk.bold(mysql_db_password) + " and database name " + chalk.bold(mysql_db_name));
+
+    // start the container in the background ("docker-compose -f docker/mysql.yml up -d")
+    shelljs.exec('docker-compose -f docker/mysql.yml up -d');
+} else {
+    mysql_db_host = await p.text({
+        message: 'MySQL host',
+        placeholder: 'localhost'
+    }) as string;
+
+    mysql_db_port = await p.text({
+        message: 'MySQL port',
+        placeholder: '3306'
+    }) as string;
+
+    mysql_db_user = await p.text({
+        message: 'MySQL user',
+        placeholder: 'root'
+    }) as string;
+
+    mysql_db_password = await p.text({
+        message: 'MySQL password',
+        placeholder: 'password'
+    }) as string;
+
+    mysql_db_name = await p.text({
+        message: 'MySQL database name',
+        placeholder: 'kitforstartups-mysql'
+    }) as string;
+}
+
+// no postgres support yet
+
+// use turso?
+let turso_db_url = "";
+let turso_auth_token = "";
+
+let use_turso = await p.confirm({
+    message: 'Do you want to setup Turso now?',
+    initialValue: true
+});
+
+if (use_turso) {
+    // get the auth token
+    turso_auth_token = await p.text({
+        message: 'Turso auth token'
+    }) as string;
+
+    // get the database url
+    turso_db_url = await p.text({
+        message: 'Turso database URL'
+    }) as string;
+}
+
+let use_github_oauth = await p.confirm({
+    message: 'Do you want to setup GitHub OAuth now?',
+    initialValue: true
+});
+
+let github_client_id = '';
+let github_client_secret = '';
+
+if (use_github_oauth) {
+    github_client_id = await p.text({
+        message: 'GitHub OAuth client ID'
+    }) as string;
+
+    github_client_secret = await p.text({
+        message: 'GitHub OAuth client secret'
+    }) as string;
+}
+
+let use_google_oauth = await p.confirm({
+    message: 'Do you want to setup Google OAuth now?',
+    initialValue: true
+});
+
+let google_oauth_client_id = '';
+let google_oauth_client_secret = '';
+let google_oauth_redirect_uri = '';
+
+if (use_google_oauth) {
+    google_oauth_client_id = await p.text({
+        message: 'Google OAuth client ID'
+    }) as string;
+
+    google_oauth_client_secret = await p.text({
+        message: 'Google OAuth client secret'
+    }) as string;
+
+    google_oauth_redirect_uri = await p.text({
+        message: 'Google OAuth redirect URI'
+    }) as string;
+}
+
+let use_email = await p.confirm({
+    message: 'Do you want to setup email now?',
+    initialValue: true
+});
+
+let email_resend_api_key = '';
+
+if (use_email) {
+    email_resend_api_key = await p.text({
+        message: 'Email resend API key'
+    }) as string;
+}
+
+let env_file = `# DATABASE
+ENABLE_DRIZZLE_LOGGER=true
+
+## MySQL
+MYSQL_DB_HOST=${mysql_db_host}
+MYSQL_DB_PORT=${mysql_db_port}
+MYSQL_DB_USER=${mysql_db_user}
+MYSQL_DB_PASSWORD=${mysql_db_password}
+MYSQL_DB_NAME=${mysql_db_name}
+
+## POSTGRES
+POSTGRES_DB_HOST=
+POSTGRES_DB_PORT=
+POSTGRES_DB_USER=
+POSTGRES_DB_PASSWORD=
+POSTGRES_DB_NAME=
+POSTGRES_MAX_CONNECTIONS=
+
+## TURSO
+TURSO_DB_URL=${turso_db_url}
+TURSO_AUTH_TOKEN=${turso_auth_token}
+
+# OAUTH
+GITHUB_CLIENT_ID=${github_client_id}
+GITHUB_CLIENT_SECRET=${github_client_secret}
+
+GOOGLE_OAUTH_CLIENT_ID=${google_oauth_client_id}
+GOOGLE_OAUTH_CLIENT_SECRET=${google_oauth_client_secret}
+GOOGLE_OAUTH_REDIRECT_URI=${google_oauth_redirect_uri}
+
+# EMAIL
+RESEND_API_KEY=${email_resend_api_key}
+`;
+
+fs.writeFileSync('.env', env_file);
+
+if (await p.confirm({
+    message: 'Do you want to start mailhog?',
+    initialValue: true
+})) {
+    shelljs.exec('docker-compose -f docker/mailhog.yml up -d');
+}
+
+clenaup();
